c52d5f9de4f9eeea264575a670257ace6c7077d3
Mubashir Nasir <116088400+mubashirnas1r@users.noreply.github.com>
2023-08-26 16:34:31 +0500

    Update api.py

--- api.py
+++ api.py
@@ -6,5 +6,4 @@ app = FastAPI()
 # Define a route for the root endpoint
 @app.get("/racingapi")
 async def read_root():
-    return {"username": "JYkAlGGbOQSAaInFFk6vQKnH",
-            "password": "Z0ucEe7ckXqLXfEeEPjOHjwh"}
+    return {"allow": true}
